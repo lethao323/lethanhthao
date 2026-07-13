662ef6d31be334dcf1e7948623bd803c5bf319f8
Lê Thành Thảo <79634248+lethao323@users.noreply.github.com>
2026-07-14 05:39:25 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,6 +1,5 @@
 `default_nettype none
 
-// Module MUX 2:1 cơ sở
 module mux2to1 (
     input wire i0,
     input wire i1,
@@ -10,7 +9,6 @@ module mux2to1 (
     assign y = sel ? i1 : i0;
 endmodule
 
-// Top Module 
 module tt_um_thao_4to1_mux (
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
@@ -21,13 +19,11 @@ module tt_um_thao_4to1_mux (
     input  wire       clk,
     input  wire       rst_n
 );
-    // Logic kết nối
     wire y01, y23;
     mux2to1 m1 (.i0(ui_in[0]), .i1(ui_in[1]), .sel(ui_in[4]), .y(y01));
     mux2to1 m2 (.i0(ui_in[2]), .i1(ui_in[3]), .sel(ui_in[4]), .y(y23));
     mux2to1 m3 (.i0(y01), .i1(y23), .sel(ui_in[5]), .y(uo_out[0]));
 
-    // Gán các chân không dùng
     assign uo_out[7:1] = 7'b0;
     assign uio_out = 8'b0;
     assign uio_oe  = 8'b0;
