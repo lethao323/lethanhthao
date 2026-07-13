f1406823e4dcb990046ea4746e1607ad0e367931
Lê Thành Thảo <79634248+lethao323@users.noreply.github.com>
2026-07-14 05:41:58 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,9 +1,10 @@
 import cocotb
 from cocotb.triggers import Timer
+import os
 
 @cocotb.test()
 async def test_mux_4to1(dut):
-    # Mapping: ui_in[5:4] là S, ui_in[3:0] là I
+    # Mapping tín hiệu
     test_cases = [
         (0, 0, 0, 0, 0, 1, 1), # Chọn I0
         (0, 1, 0, 0, 1, 0, 1), # Chọn I1
@@ -14,5 +15,6 @@ async def test_mux_4to1(dut):
     for s1, s0, i3, i2, i1, i0, expected in test_cases:
         dut.ui_in.value = (s1 << 5) | (s0 << 4) | (i3 << 3) | (i2 << 2) | (i1 << 1) | i0
         await Timer(1, units="us")
-        assert dut.uo_out[0].value == expected, f"Lỗi logic tại S1={s1}, S0={s0}"
-    print("Mạch hoạt động hoàn hảo!")
+        assert dut.uo_out[0].value == expected, f"Lỗi logic S1={s1}, S0={s0}"
+    
+    print("Test passed!")
